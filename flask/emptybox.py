5c06bbdf02681bb02131d763b78ce26530a83c92
giovanism <giovanism@outlook.co.id>
2019-08-28 17:39:50 +0700

    flask: Implement url upload

--- flask/emptybox.py
+++ flask/emptybox.py
@@ -1,6 +1,10 @@
+import urllib3
 import boto3
+import uuid
+import io
 import os
 from flask import Flask, redirect, request
+from PIL import Image
 
 # Settings
 app = Flask(__name__)
@@ -15,6 +19,14 @@ s3 = boto3.client('s3', endpoint_url=f'http://{S3_HOST}/',
                   aws_secret_access_key=S3_SECRET_KEY)
 
 
+# Utils
+
+def gen_filename(fmt):
+    return f'{uuid.uuid4()}.{fmt.lower()}'
+
+
+# Handlers
+
 @app.route('/')
 def handle_default():
     return redirect(MAIN_SITE)
@@ -22,24 +34,40 @@ def handle_default():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'file' in request.files:
-        file = request.files['file']
+    try:
+        if request.form['type'] == 'file':
+            file = request.files['file']
+            body = file.stream
 
-        response = s3.put_object(Bucket=S3_BUCKET,
-                                 Key=file.filename,
-                                 Body=file.stream)
+        elif request.form['type'] == 'url':
+            url = request.form['url']
+            pool = urllib3.connection_from_url(url)
+            response = pool.request('GET', url)
+            body = io.BytesIO(response.data)
 
-        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-            app.logger.info(response)
-            return 'Error', 400
         else:
-            return 'Saved'
+            raise KeyError
 
-    else:
-        err = 'No file provided'
+    except KeyError:
+        err = 'Invalid request'
         app.logger.debug(err)
 
         return err, 400
+
+    img = Image.open(body)
+    key = gen_filename(img.format)
+
+    body.seek(0)
+
+    response = s3.put_object(Bucket=S3_BUCKET,
+                             Key=key,
+                             Body=body)
+
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        app.logger.info(response)
+        return 'Error', 400
+
+    return 'Saved'
 
 
 @app.route('/stats')
